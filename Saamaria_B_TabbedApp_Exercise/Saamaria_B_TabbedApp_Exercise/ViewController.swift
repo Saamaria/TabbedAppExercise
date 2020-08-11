caf4955f6a3af4740d7dbfef0c5a9ef739eaa8fe
Saamaria <67989662+Saamaria@users.noreply.github.com>
2020-08-11 01:50:19 -0500

    Add files via upload

---
+++ Saamaria_B_TabbedApp_Exercise/Saamaria_B_TabbedApp_Exercise/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Saamaria_B_TabbedApp_Exercise
+//
+//  Created by Saamaria Bryant on 8/11/20.
+//  Copyright © 2020 Tabbed App Exercise. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
